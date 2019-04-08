769f310ab734fb935c35ae5abdb5d91064ef6186
Jan Gerling <jan.gerling@posteo.de>
2019-04-08 15:37:24 +0200

    Fix color formatting bug

--- astor_runner.sh
+++ astor_runner.sh
@@ -13,7 +13,7 @@ tests=$(ls $1)
 # iterate over all tests given in the directory
 for currenttest in $tests; do
 	echo -e "\e[32m\n\n\n\n* * * * * * * * New * * * * * * * *"
-	echo -e "FILE:" $currenttest "\n"
+	echo -e "\e[39mFILE:" $currenttest "\n"
 	
 	cd $currenttest
 	mvn clean compile test |& tee results/$currenttest"-build-output"
@@ -25,7 +25,7 @@ for currenttest in $tests; do
 		
 		# iterate over all of the three scopes
 		for scope in ${scopes[@]}; do		
-			echo -e "\n\n\FILE:\e[34m" $currenttest " run with mode: " $mode " in scope: " $scope "\n"
+			echo -e "\n\n\FILE:\e[34m" $currenttest " run with mode: " $mode " in scope: " $scope "\e[39m \n"
 		
 			outputFileName=$currenttest"-"$mode"-"$scope
 			java -cp $(cat /tmp/astor-classpath.txt):target/classes fr.inria.main.evolution.AstorMain -scope $scope -jvm4testexecution $jvmPath -mode $mode -srcjavafolder /src/java/ -srctestfolder /src/test/  -binjavafolder /target/classes/ -bintestfolder  /target/test-classes/ -location $currenttest -dependencies ./examples/libs/junit-4.4.jar -flthreshold $treshold -seed $seedValue -maxtime $maxTime -stopfirst true |& tee results/$outputFileName
