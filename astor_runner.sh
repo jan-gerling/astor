b6def6632bbc9f3d166147edc0f6f345733837fb
Jan Gerling <jan.gerling@posteo.de>
2019-04-08 15:35:02 +0200

    Better output formatting

--- astor_runner.sh
+++ astor_runner.sh
@@ -10,22 +10,23 @@ jvmPath="/usr/lib/jvm/java-1.7.0-openjdk-amd64/bin"
 
 tests=$(ls $1)
 
-
 # iterate over all tests given in the directory
 for currenttest in $tests; do
-	echo -e "\n* * * * * * * * New * * * * * * * *"
+	echo -e "\e[32m\n\n\n\n* * * * * * * * New * * * * * * * *"
 	echo -e "FILE:" $currenttest "\n"
 	
 	cd $currenttest
-	mvn clean compile test |& tee results/$=$currenttest"-build-output"
+	mvn clean compile test |& tee results/$currenttest"-build-output"
 	cd ~/astor/
 	mvn dependency:build-classpath -B | egrep -v "(^\[INFO\]|^\[WARNING\])" | tee /tmp/astor-classpath.txt
 	
 	# iterate over all of the three modes
 	for mode in ${modes[@]}; do
-	
+		
 		# iterate over all of the three scopes
-		for scope in ${scopes[@]}; do			
+		for scope in ${scopes[@]}; do		
+			echo -e "\n\n\FILE:\e[34m" $currenttest " run with mode: " $mode " in scope: " $scope "\n"
+		
 			outputFileName=$currenttest"-"$mode"-"$scope
 			java -cp $(cat /tmp/astor-classpath.txt):target/classes fr.inria.main.evolution.AstorMain -scope $scope -jvm4testexecution $jvmPath -mode $mode -srcjavafolder /src/java/ -srctestfolder /src/test/  -binjavafolder /target/classes/ -bintestfolder  /target/test-classes/ -location $currenttest -dependencies ./examples/libs/junit-4.4.jar -flthreshold $treshold -seed $seedValue -maxtime $maxTime -stopfirst true |& tee results/$outputFileName
 		done	
