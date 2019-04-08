2fa1f12f5e58575908f2fffedcd9e98bc5d85d24
Jan Gerling <jan.gerling@posteo.de>
2019-04-08 18:44:36 +0200

    Fix path

--- astor_runner.sh
+++ astor_runner.sh
@@ -18,10 +18,10 @@ for currenttest in $tests; do
 	fullPath=$1/$currenttest
 
 	echo -e "\e[32m\n\n\n\n* * * * * * * * New * * * * * * * *"
-	echo -e "\e[39mFILE:" fullPath "\n"
+	echo -e "\e[39mFILE:" $fullPath "\n"
 	
 	#compile and current build test case
-	cd fullPath
+	cd $fullPath
 	mvn clean compile test |& tee results/$currenttest"-build-output"
 	cd $2
 	mvn dependency:build-classpath -B | egrep -v "(^\[INFO\]|^\[WARNING\])" | tee /tmp/astor-classpath.txt
@@ -32,15 +32,15 @@ for currenttest in $tests; do
 		# iterate over all of the three scopes
 		for scope in ${scopes[@]}; do		
 			echo -e "\n\n\e[35mFILE:" $currenttest " run with mode: " $mode " in scope: " $scope "\n"
-			echo -e "java -cp" $(cat /tmp/astor-classpath.txt)":target/classes fr.inria.main.evolution.AstorMain -scope " $scope "-jvm4testexecution" $jvmPath "-mode " $mode "-srcjavafolder /src/java/ -srctestfolder /src/test/ -binjavafolder /target/classes/ -bintestfolder /target/test-classes/ -location "fullPath "-dependencies ./examples/libs/junit-4.4.jar -flthreshold "$treshold "-seed "$seedValue "-maxtime "$maxTime "-stopfirst true |& tee results/$outputFileName"
+			echo -e "java -cp" $(cat /tmp/astor-classpath.txt)":target/classes fr.inria.main.evolution.AstorMain -scope " $scope "-jvm4testexecution" $jvmPath "-mode " $mode "-srcjavafolder /src/java/ -srctestfolder /src/test/ -binjavafolder /target/classes/ -bintestfolder /target/test-classes/ -location "$fullPath "-dependencies ./examples/libs/junit-4.4.jar -flthreshold "$treshold "-seed "$seedValue "-maxtime "$maxTime "-stopfirst true |& tee results/$outputFileName"
 			echo -e "\e[39m"
 		
 			outputFileName=$currenttest"-"$mode"-"$scope
-			java -cp $(cat /tmp/astor-classpath.txt):target/classes fr.inria.main.evolution.AstorMain -scope $scope -jvm4testexecution $jvmPath -mode $mode -srcjavafolder /src/java/ -srctestfolder /src/test/ -binjavafolder /target/classes/ -bintestfolder /target/test-classes/ -location fullPath -dependencies ./examples/libs/junit-4.4.jar -flthreshold $treshold -seed $seedValue -maxtime $maxTime -stopfirst true |& tee results/$outputFileName
+			java -cp $(cat /tmp/astor-classpath.txt):target/classes fr.inria.main.evolution.AstorMain -scope $scope -jvm4testexecution $jvmPath -mode $mode -srcjavafolder /src/java/ -srctestfolder /src/test/ -binjavafolder /target/classes/ -bintestfolder /target/test-classes/ -location $fullPath -dependencies ./examples/libs/junit-4.4.jar -flthreshold $treshold -seed $seedValue -maxtime $maxTime -stopfirst true |& tee results/$outputFileName
 		done	
 	done
 	
 	#move completed test cases to other directory
-	mv fullPath $2/results/done
+	mv $fullPath $2/results/done
     echo -e "- - - - - - - - END - - - - - - - -\n"
 done
