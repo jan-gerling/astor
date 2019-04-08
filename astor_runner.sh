66fed47e346d98114bb0b859f06842002951e482
Jan Gerling <jan.gerling@posteo.de>
2019-04-08 16:01:16 +0200

    Better color formatting

--- astor_runner.sh
+++ astor_runner.sh
@@ -25,7 +25,7 @@ for currenttest in $tests; do
 		
 		# iterate over all of the three scopes
 		for scope in ${scopes[@]}; do		
-			echo -e "\n\n\ \e[34mFILE:" $currenttest " run with mode: " $mode " in scope: " $scope "\n"
+			echo -e "\n\n\e[35mFILE:" $currenttest " run with mode: " $mode " in scope: " $scope "\n"
 			echo -e "java -cp" $(cat /tmp/astor-classpath.txt)":target/classes fr.inria.main.evolution.AstorMain -scope " $scope "-jvm4testexecution" $jvmPath "-mode " $mode "-srcjavafolder /src/java/ -srctestfolder /src/test/ -binjavafolder /target/classes/ -bintestfolder /target/test-classes/ -location "$currenttest "-dependencies ./examples/libs/junit-4.4.jar -flthreshold "$treshold "-seed "$seedValue "-maxtime "$maxTime "-stopfirst true |& tee results/$outputFileName"
 			echo -e "\e[39m"
 		
