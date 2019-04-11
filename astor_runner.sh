67d72270a6f58f8159d64723a05e5b4c733ff6a7
Jan Gerling <jan.gerling@posteo.de>
2019-04-11 20:57:05 +0200

    Fix Execution Summary Selection

--- astor_runner.sh
+++ astor_runner.sh
@@ -65,15 +65,15 @@ for currenttest in $tests; do
 
 			if  [ -f "$outputFile" ] && grep -q "$successString" "$outputFile" ; then
 				echo -e "[\e[32mSUCCESS\e[39m]: $runname found a fix in $runTime seconds!\n" |& tee -a "$runSummary"
-				summaryInfo=$(sed -n -e '/----SUMMARY_EXECUTION---/,$' "$outputFile")
+				summaryInfo=$(cat "$outputFile" | sed -n -e '/----SUMMARY_EXECUTION---/,$')
 				echo -e "$summaryInfo" |& tee -a "$runSummary"
 			elif [ -f "$outputFile" ] && grep -q "Exception" "$outputFile" ; then				
 				echo -e "[\e[31m[EXCEPTION\e[39m]: $runname had an exception: $exceptionInfo\n" |& tee -a "$runSummary"
-				exceptionInfo=$(sed -n -e '/Exception/,$' "$outputFile")
+				exceptionInfo=$(cat "$outputFile" | sed -n -e '/Exception/,$')
 				echo -e "$exceptionInfo" |& tee -a "$runSummary"
 			elif [ -f "$outputFile" ] && grep -q "$timeString" "$outputFile" ; then
 				echo -e "[\e[33m[WARNING\e[39m]: $runname did not find a fix in $runTime seconds!\n" |& tee -a "$runSummary"
-				summaryInfo=$(sed -n -e '/----SUMMARY_EXECUTION---/,$' "$outputFile")
+				summaryInfo=$(cat "$outputFile" | sed -n -e '/----SUMMARY_EXECUTION---/,$')
 				echo -e "$summaryInfo" |& tee -a "$runSummary"	 		
 			else 
 				echo -e "[\e[31mFAILURE\e[39m]: $runname did not finish properly!\n" |& tee -a "$runSummary"
