c11481a498b358a078a8fb829a525a37cf06c5a0
Jan Gerling <jan.gerling@posteo.de>
2019-04-11 14:30:14 +0200

    Smart rerun of astor

--- astor_runner.sh
+++ astor_runner.sh
@@ -2,17 +2,28 @@
 # first argument: full path to directory to crawl, e.g. ~/astor/defect4j_tests/math70
 # second argument: full path to astor main directory, e.g. ~/astor/
 
-mkdir $2/results $2/results/done
-
+resultsDir=$2results
 modes=(jmutrepair jkali jgenprog)
-scopes=(File Package Application)
+scopes=(local package global)
 seedValue=10
 treshold=0.5
 maxTime=100
+#local path to your junit executable
 junitPath="./examples/libs/junit-4.4.jar"
+#abosulte path to the jre 7, used for defect4j
 jvmPath="/usr/lib/jvm/java-1.7.0-openjdk-amd64/bin"
-
 tests=$(ls $1)
+
+#create results dir if necessary
+if [ ! -d $resultsDir ]; then
+	mkdir $resultsDir
+	echo -e "[INFO] created $resultsDir to store results"
+fi
+
+#build astor
+echo -e "\n\n\e[35m [BUILD] astor in cd $2 \e[39m\n"
+cd $2
+mvn clean compile
 
 # iterate over all tests given in the directory
 for currenttest in $tests; do
@@ -23,7 +34,7 @@ for currenttest in $tests; do
 	
 	#compile and current build test case
 	cd $fullPath
-	mvn clean compile test |& tee results/$currenttest"-build-output"
+	mvn clean compile test |& tee "$resultsDir/$currenttest-build-output.txt"
 	cd $2
 	mvn dependency:build-classpath -B | egrep -v "(^\[INFO\]|^\[WARNING\])" | tee /tmp/astor-classpath.txt
 	
@@ -31,18 +42,21 @@ for currenttest in $tests; do
 	for mode in ${modes[@]}; do
 		
 		# iterate over all of the three scopes
-		for scope in ${scopes[@]}; do		
-			outputFileName=$currenttest"-"$mode"-"$scope
-		
-			echo -e "\n\n\e[35mFILE:" $currenttest" run with mode: " $mode" in scope:" $scope "\n"
-			echo -e "java -cp" $(cat /tmp/astor-classpath.txt)":target/classes fr.inria.main.evolution.AstorMain -scope" $scope "-jvm4testexecution" $jvmPath "-mode" $mode "-srcjavafolder /src/java/ -srctestfolder /src/test/ -binjavafolder /target/classes/ -bintestfolder /target/test-classes/ -location" $fullPath "-dependencies" $junitPath "-flthreshold" $treshold "-seed" $seedValue "-maxtime" $maxTime "-stopfirst true |& tee results/$outputFileName"
-			echo -e "\e[39m"
+		for scope in ${scopes[@]}; do	
+			runname=$currenttest"-"$mode"-"$scope
+			outputFile=$resultsDir"/"$runname".txt"
 			
-			java -cp $(cat /tmp/astor-classpath.txt):target/classes fr.inria.main.evolution.AstorMain -scope $scope -jvm4testexecution $jvmPath -mode $mode -srcjavafolder /src/java/ -srctestfolder /src/test/ -binjavafolder /target/classes/ -bintestfolder /target/test-classes/ -location $fullPath -dependencies $junitPath -flthreshold $treshold -seed $seedValue -maxtime $maxTime -stopfirst true |& tee results/$outputFileName
+			# check if this test was already run with the current configuration
+			echo -e "$outputFile"
+			if  [ ! -f "$outputFile" ] || [ grep -Fxq "[SUCCESS] for $runname" "$outputFile"]; then
+				echo -e "\n\n\e[35m [RUN] $runname \e[39m\n"
+			
+				java -cp $(cat /tmp/astor-classpath.txt):target/classes fr.inria.main.evolution.AstorMain -jvm4testexecution $jvmPath -mode $mode -scope $scope -srcjavafolder /src/java/ -srctestfolder /src/test/ -binjavafolder /target/classes/ -bintestfolder /target/test-classes/ -location $fullPath -dependencies $junitPath -flthreshold $treshold -seed $seedValue -maxtime $maxTime -stopfirst true |& tee outputFile
+				echo "[SUCCESS] for $runname" |& tee outputFile
+			else
+				echo -e "\n\n\e[33m [WARNING]: $runname was already done! \e[39m \n"
+			fi
 		done	
 	done
-	
-	#move completed test cases to other directory
-	mv $fullPath $2/results/done
     echo -e "- - - - - - - - END - - - - - - - -\n"
 done
