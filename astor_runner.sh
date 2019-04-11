766c33c8cbb26a426bb52b88f8d95ebe1fb9805d
Jan Gerling <jan.gerling@posteo.de>
2019-04-11 16:36:48 +0200

    Update astor_runner.sh

--- astor_runner.sh
+++ astor_runner.sh
@@ -50,7 +50,7 @@ for currenttest in $tests; do
 			if  [ ! -f "$outputFile" ] || [ grep -Fxq "[SUCCESS] for $runname" "$outputFile"]; then
 				echo -e "\n\n\e[35m[RUN] $runname \e[39m\n"
 			
-				java -cp $(cat /tmp/astor-classpath.txt):target/classes fr.inria.main.evolution.AstorMain -jvm4testexecution $jvmPath -mode $mode -scope $scope -srcjavafolder /src/java/ -srctestfolder /src/test/ -binjavafolder /target/classes/ -bintestfolder /target/test-classes/ -location $fullPath -dependencies $junitPath -flthreshold $treshold -seed $seedValue -maxtime $maxTime -stopfirst true |& tee "$outputFile"
+				java -cp $(cat /tmp/astor-classpath.txt):target/classes fr.inria.main.evolution.AstorMain -jvm4testexecution $jvmPath -mode $mode -scope $scope -srcjavafolder /src/java/ -srctestfolder /src/test/ -binjavafolder /target/classes/ -bintestfolder /target/test-classes/ -location $fullPath -dependencies $junitPath -flthreshold $treshold -maxtime $maxTime -stopfirst true |& tee "$outputFile"
 			else
 				echo -e "\n\n\e[33m[WARNING]: $runname was already done! \e[39m \n"
 			fi
